a354aa1e2cab4b010477201f99410232ef61657a
Rumit m Gevariya <77478366+RumitGevariya@users.noreply.github.com>
2021-01-15 17:33:03 +0530

    Create staticmethod.py

---
+++ staticmethod.py
@@ -0,0 +1,10 @@
+#static method:
+class B:
+    def __init__(self):
+        print('one')
+    def __init__(self):
+        print('two')
+    def printinfo():
+        print('hello class')
+obj =B()
+B.printinfo()        
